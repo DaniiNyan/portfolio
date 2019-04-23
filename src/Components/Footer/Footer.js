0efe70f42b7e25551e39405a6e25aa107f0eab36
daniinyan <daniela.dbass@gmail.com>
2019-04-23 19:50:57 -0300

    Termina footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import FooterStyle from "./Footer.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 const footer = props => {
+    const [redesState, setRedesState] = useState({
+        redes: [
+        { icon: faGithub, url: "https://github.com/DaniiNyan" },
+        { icon: faLinkedinIn, url: "https://github.com/DaniiNyan" },
+        { icon: faFacebookF, url: "https://github.com/DaniiNyan" }
+      ]
+    });
 
-    return (
-        <div>
-            <footer className={FooterStyle.rodape}>
-                <div>
-                    <ul className={FooterStyle.listaIcones}>
-                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                    </ul>
-                </div>
 
-                Desenvolvido por @daniinyan
-            </footer>
+  const renderRedes = () => (
+      <div>
+        {redesState.redes.map(redeSocial => (
+            <a href={redeSocial.url} className={FooterStyle.icone}>
+              <FontAwesomeIcon icon={redeSocial.icon} size="2x" color="white" />
+            </a>
+          )
+        )}
+      </div>
+    );
 
+  return (
+    <div>
+      <footer className={FooterStyle.rodape}>
+        <div className={FooterStyle.listaIcones}>
+        {renderRedes()}
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
 
 export default footer;
