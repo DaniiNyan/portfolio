5b813ff0a51f4429252a1db74db97548b2d3f3c7
daniinyan <daniela.dbass@gmail.com>
2019-04-26 19:41:34 -0300

    Updates

---
+++ precache-manifest.ba83ae74acd2b8f2f25311769e650f8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "85397cff2428e3dd9fc303ec140ee3bf",
+    "url": "/portfolio/static/media/fotoDani.85397cff.jpg"
+  },
+  {
+    "revision": "be087265d40a99243b67911af308b038",
+    "url": "/portfolio/static/media/bkgHeader.be087265.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e762290ae173ce1ca798",
+    "url": "/portfolio/static/js/main.9c91c562.chunk.js"
+  },
+  {
+    "revision": "84004f8a2cee8339fa1d",
+    "url": "/portfolio/static/js/2.de79f65e.chunk.js"
+  },
+  {
+    "revision": "e762290ae173ce1ca798",
+    "url": "/portfolio/static/css/main.b81aab81.chunk.css"
+  },
+  {
+    "revision": "84004f8a2cee8339fa1d",
+    "url": "/portfolio/static/css/2.86a3ac4d.chunk.css"
+  },
+  {
+    "revision": "4da0e7a78412217c8e4a9def44280192",
+    "url": "/portfolio/index.html"
+  }
+];
